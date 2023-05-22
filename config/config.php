fa7efc79653f49d876ad038e76d8a48c0ce6e7ea
Liberty Vasquez <201980193@psu.palawan.edu.ph>
2023-05-23 04:31:10 +0800

    done editing logApp source code

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost/logApp-vasquez/index.php');
+	define('DB_HOST', 'sql12.freesqldatabase.com');
+	define('DB_USER', 'sql12620190');
+	define('DB_PASS', '9F8UREqjlp');
+	define('DB_NAME', 'sql12620190');
---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <p>Created by <a href="https://github.com/liberty-v">libertyvasquez</a> for BSIT3 B1</p>
+    </div>
+</footer>
+  </body>
+</html>
